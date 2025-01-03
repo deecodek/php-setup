b965a7035e37ef17ad0dbe282ac6e118647c4966
Deepak Repositories <deepakddm1@gmail.com>
2025-01-04 01:56:42 +0530

    minor changes

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,22 +2,9 @@
 
 set -e
 
-PHP_VERSION=$1
-PHP_EXTENSIONS=$2
+# Get the PHP version and extensions from inputs
+PHP_VERSION="${INPUT_PHP-VERSION}"
+EXTENSIONS="${INPUT_EXTENSIONS}"
 
-# Install the specified PHP version
-if [ "$PHP_VERSION" = "latest" ]; then
-    apt-get install -y php
-else
-    apt-get install -y php"$PHP_VERSION"
-fi
-
-# Install PHP extensions if specified
-if [ -n "$PHP_EXTENSIONS" ]; then
-    IFS=',' read -r -a extensions <<< "$PHP_EXTENSIONS"
-    for ext in "${extensions[@]}"; do
-        apt-get install -y php"$PHP_VERSION"-"$ext"
-    done
-fi
-
-echo "PHP $PHP_VERSION is set up with the following extensions: $PHP_EXTENSIONS"
+# Update PHP version (if necessary)
+echo "Installing PHP $PHP_VERSION with extensions: $EXTENSIONS"
